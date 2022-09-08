a3e5644454d8d66503f46cae828aa7a834393313
cpsanpedro <cpsanpedro@yahoo.com>
2022-09-08 12:24:45 +0800

    add edit delete reducer test
    pushed supposed to be discarded changes

--- test/reducers_test.dart
+++ test/reducers_test.dart
@@ -5,26 +5,44 @@ import 'package:todo_redux/model/model.dart';
 import 'package:todo_redux/redux/actions.dart';
 import 'package:todo_redux/redux/reducers.dart';
 
+ToDoItem get mockToDoItem => ToDoItem((item) => item
+  ..id = "1"
+  ..title = "Item 1");
+
+ToDoItem get updatedMockToDoItem => ToDoItem((item) => item
+  ..id = "1"
+  ..title = "Updated Item 1");
+
 AppState mockTodo() {
-  return AppState((b) => b
-    ..items = ListBuilder<ToDoItem>([
-      ToDoItem((item) => item
-        ..id = DateTime.now().toString().substring(0, 19)
-        ..title = "Item 1")
-    ]));
+  return AppState((b) => b..items = ListBuilder<ToDoItem>([mockToDoItem]));
+}
+
+AppState updatedMockTodo() {
+  return AppState(
+      (b) => b..items = ListBuilder<ToDoItem>([updatedMockToDoItem]));
 }
 
 void main() {
+  var store = Store<AppState>(appReducer, initialState: AppState.init());
   test('initial empty todo list', () {
-    var store = Store<AppState>(appReducer, initialState: AppState.init());
     expect(store.state, AppState.init());
   });
 
   test('add one todo', () {
-    var store = Store<AppState>(appReducer, initialState: AppState.init());
     store.dispatch(AddItemAction((b) => b
-      ..id = DateTime.now().toString().substring(0, 19)
-      ..title = "Item 1"));
+      ..id = mockToDoItem.id
+      ..title = mockToDoItem.title));
     expect(store.state, mockTodo());
+  });
+
+  test('update one todo', () {
+    store.dispatch(
+        UpdateItemAction((b) => b.item = updatedMockToDoItem.toBuilder()));
+    expect(store.state, updatedMockTodo());
+  });
+
+  test('delete one todo', () {
+    store.dispatch(DeleteItemAction((b) => b.item = mockToDoItem.toBuilder()));
+    expect(store.state, AppState.init());
   });
 }
